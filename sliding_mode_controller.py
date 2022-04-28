17029de5f025a290c8e22f504a33965b7d341c8f
malpurepiyush07@gmail.com <malpurepiyush07@gmail.com>
2022-04-28 18:15:16 -0400

    updated with desired angular trajectory equations

--- sliding_mode_controller.py
+++ sliding_mode_controller.py
@@ -14,7 +14,21 @@ ten6 = 10**-6
 m, l, Ix, Iy, Iz, Ip  = 0.027, 0.046, 16.5717*ten6, 16.5717*ten6, 29.2616*ten6, 12.65625*ten6
 kF, kM, w_max, w_min, g = 1.26192*10**-8, 5.9645*10**-3, 2618, 0, 9.81
 
+# TODO::
+# Get all desired trajectories from the previous code
+
+
+# Defining desired angular componenets
+phi_d, dphi_d, dtheta_d, dpsi_d, ddphi_d, ddtheta_d, ddpsi_d = 0 
 t1,t2,t3 = 1/(4*kF), math.sqrt(2)/(4*kF*l), 1/(4*kM*kF)
+
+# We have x_d, y_d, z_d we calculate the theta_d and phi_d from the relations given between x,y,z
+Kp, Kd = 0.1, 0.1 # PD control Kp, Kd
+Fx = m * (-Kp * (x - x_d) - Kd * (dx-dx_d) + ddx_d)
+Fy = m * (-Kp * (y - y_d) - Kd * (dy-dy_d) + ddy_d)
+theta_d = sympy.asin(Fx/u1)
+phi_d = sympy.asin(-Fy/u1)
+
 w1 = sympy.sqrt( Matrix([t1,-t2,-t2,-t3]).dot( Matrix([u1,u2,u3,u4])) )
 w2 = sympy.sqrt( Matrix([t1,-t2, t2, t3]).dot( Matrix([u1,u2,u3,u4])) )
 w3 = sympy.sqrt( Matrix([t1, t2, t2,-t3]).dot( Matrix([u1,u2,u3,u4])) )
@@ -22,9 +36,9 @@ w4 = sympy.sqrt( Matrix([t1, t2,-t2, t3]).dot( Matrix([u1,u2,u3,u4])) )
 
 Omega = w1-w2+w3-w4
 
-# laambda = 1
-# e = q - qd
-# de = dq - dqd
+laambda = 1
+e = q - qd
+de = dq - dqd
 
 ddx = (u1/m) * ( cos(phi)*sin(theta)*cos(psy) + sin(phi)*sin(psy) )
 ddy = (u1/m) * ( cos(phi)*sin(theta)*cos(psy) - sin(phi)*sin(psy) )
@@ -33,7 +47,7 @@ ddphi = dtheta*dpsy + (Iy-Iz)/Ix - Ip/Ix*Omega*dtheta + 1/Ix * u2
 ddtheta = dphi*dpsy + (Iz-Ix)/Iy - Ip/Iy*Omega*dphi + 1/Iy * u3
 ddpsy = dtheta*dphi + (Ix-Iy)/Iz - 1/Iz * u4
 
+#s = de + laambda * e
 
-
-# s = de + laambda * e
+# Define sliding control law for z, phi, theta, psy
 
